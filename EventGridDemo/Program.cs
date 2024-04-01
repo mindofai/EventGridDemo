4553496e093a7bc03d733853c31b655c9c2d5154
Bryan Anthony Garcia <anthonyarenas707@gmail.com>
2024-04-01 20:26:56 +1100

    Add project files.

---
+++ EventGridDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using Azure;
+using Azure.Messaging.EventGrid;
+
+string topicEndpoint = "<event grid topic endpoint>";
+string topicAccessKey = "<event grid topic key>";
+
+EventGridPublisherClient client = new EventGridPublisherClient(
+    new Uri(topicEndpoint),
+    new AzureKeyCredential(topicAccessKey));
+
+EventGridEvent egEvent =
+    new EventGridEvent(
+        "ExampleEventSubject",
+        "Example.CarPurchased",
+        "1.0",
+        "Hi Everyone!");
+
+// Send the event
+await client.SendEventAsync(egEvent);
